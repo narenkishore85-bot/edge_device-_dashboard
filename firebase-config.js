c1a35075de576e59500bc1b11f0ec7ff8fd019c9
Naren Kishore <narenkishore85@gmail.com>
2026-08-29 20:11:00 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuSk2ZT9-teWqb9xg4o3jcBhX6HtagcWA",
+    authDomain: "edge-device-monitor.firebaseapp.com",
+    databaseURL: "https://edge-device-monitor-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "edge-device-monitor",
+    storageBucket: "edge-device-monitor.firebasestorage.app",
+    messagingSenderId: "212488454144",
+    appId: "1:212488454144:web:37f41819cbf51d1634d8e5"
+};
+
+export default firebaseConfig;
